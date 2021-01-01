a8d199ff31e93dd7e807bdb8396e44f7e2c7a4c8
mddarr <mddarr@gmail.com>
2021-01-01 01:07:27 -0800

    I perforemd a window group by!

--- spark/structured_streaming/avro_structured_streaming.py
+++ spark/structured_streaming/avro_structured_streaming.py
@@ -4,6 +4,17 @@ import pyspark as ps
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroDeserializer
 from confluent_kafka.serialization import SerializationContext
+from pyspark.sql import functions as F
+
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+from pyspark.sql.functions import window
+
+windowSize = 10
+slideSize = 5
+windowDuration = '{} seconds'.format(windowSize)
+slideDuration = '{} seconds'.format(slideSize)
+
 
 
 def getSparkInstance():
@@ -35,14 +46,19 @@ def process_row(row):
      ]
     }
     '''
-    print("THE ROW LOOKS LIKE")
-    print(row)
+    # print("THE ROW LOOKS LIKE")
+    # print(row)
     schemaRegistryClient = SchemaRegistryClient({"url": "http://localhost:8081"})
     avroDeserializer = AvroDeserializer(schema, schemaRegistryClient)
     serializationContext = SerializationContext("time-series", schema)
     deserialized_row = avroDeserializer(row.value, serializationContext)
-    print("DESERIALIZED ROW")
-    print(str(deserialized_row))
+    # print("DESERIALIZED ROW")
+    # print(str(deserialized_row))
+
+
+def proces_grouped_by_dataframe(row):
+    print("THE ROW LOOKS LIKE")
+    print(row)
 
 
 # `from_avro` requires Avro schema in JSON string format.
@@ -52,16 +68,46 @@ streamingDF = spark\
   .format("kafka")\
   .option("kafka.bootstrap.servers", "localhost:9092")\
   .option("subscribe", "time-series")\
+  .option('includeTimestamp', 'true')\
   .load()
 
-avroDF = streamingDF\
-  .writeStream\
-  .format("kafka")\
-  .foreach(process_row)\
-  .option("checkpointLocation", "checkpoints")\
-  .option("kafka.bootstrap.servers", "localhost:9092")\
-  .option("topic", "topic2")\
-  .start()
+
+# streamingDataFrameGroupedByKeyCount = streamingDF \
+#     .withWatermark("timestamp", "1 minutes") \
+#     .groupBy(F.col("key"), "timestamp") \
+#     .count()
+
+windowedCounts = streamingDF.groupBy(
+    window(streamingDF.timestamp, windowDuration, slideDuration),
+    streamingDF.key
+).count().orderBy('window')
+
+
+windowedDF = windowedCounts\
+  .writeStream \
+    .outputMode('complete') \
+    .format('console') \
+    .option('truncate', 'false') \
+    .start()
+
+# avroDF = streamingDF\
+#   .writeStream\
+#   .format("kafka")\
+#   .foreach(proces_grouped_by_dataframe)\
+#   .start()
+
+
+
+
+
+# avroDF = streamingDF\
+#   .writeStream\
+#   .format("kafka")\
+#   .foreach(process_row)\
+#   .option("checkpointLocation", "checkpoints")\
+#   .option("kafka.bootstrap.servers", "localhost:9092")\
+#   .option("topic", "topic2")\
+#   .start()
 
 
 spark.streams.awaitAnyTermination()

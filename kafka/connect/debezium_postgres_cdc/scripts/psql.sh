078e7e8f16ee8da64262796040f04d22ba2d3f92
mddarr <mddarr@gmail.com>
2021-01-08 10:57:42 -0800

    Playing around w/ this tutorial..

--- kafka/connect/debezium_postgres_cdc/scripts/psql.sh
+++ kafka/connect/debezium_postgres_cdc/scripts/psql.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker exec -it postgres psql 'dbname=postgresdb user=postgres options=--search_path=users_schema'
+
+docker exec -it postgres psql 'dbname=postgresdb user=postgres options=--search_path=bank'
+
+#docker exec -it postgres psql 'dbname=postgresdb user=postgres options=--search_path=users_schema'
 #docker exec -it postgres psql 'dbname=postgresdb user=postgres'
